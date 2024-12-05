7c45ad4b7c41b9b570e4fcf61742dda73d582c95
snadwich <79296594+snadwich01@users.noreply.github.com>
2024-12-05 16:22:17 +0600

    Create task 3.java

---
+++ Lab stuff/Assignment 4/task 3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class countPositiveNegative {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = data.nextInt();
+        int num1 = 1;
+       
+        int countNega = 0;
+        int countPosi = 0;
+
+        while (num1 <= num) {
+            System.out.print("Enter number " + num1++ + ":");
+            int input = data.nextInt();
+            
+            if (input < 0) {
+            countNega++;
+            } else {
+            countPosi++;
+            } 
+            }
+        
+        System.out.println(countPosi  + "Non-negative Numbers");
+        System.out.println(countNega  + "Negative Numbers");
+        
+        data.close();
+    }
+}
