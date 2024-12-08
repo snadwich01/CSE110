41bdf8eb6bd1c9075a07c75ef78e291113ea7a9d
snadwich <79296594+snadwich01@users.noreply.github.com>
2024-12-08 13:32:14 +0600

    Update Task 8.java

--- Lab stuff/Assignment 4/Task 8.java
+++ Lab stuff/Assignment 4/Task 8.java
@@ -4,8 +4,8 @@ public class rectangle {
     public static void main(String[] args) {
         Scanner data = new Scanner(System.in);
 
-        System.out.print("Please enter the lentgh of the rectangle: ");
-        int lentgh = data.nextInt();
+        System.out.print("Please enter the length of the rectangle: ");
+        int length = data.nextInt();
 
         System.out.print("Please enter the width of the rectangle: ");
         int width = data.nextInt();
@@ -13,12 +13,11 @@ public class rectangle {
         System.out.println();
         
         for (int j = 1; j<=width; j++) {
-         for (int i = 1; i <= lentgh; i++){
+         for (int i = 1; i <= length; i++){
             System.out.print(i + " ");
-         }
-         System.out.println();
+         } System.out.println();
         }
-        
+
         System.out.println();   
         data.close();
     }
