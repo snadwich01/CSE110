d756656271be93d2bc9f2b44d7cae3e2f72076b4
snadwich <79296594+snadwich01@users.noreply.github.com>
2024-12-10 10:31:19 +0600

    Update Task 6.java

--- Lab stuff/Assignment 4/Task 6.java
+++ Lab stuff/Assignment 4/Task 6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class yseries {
+public class ydivCount {
     public static void main(String[] args) {
         Scanner data = new Scanner(System.in);
 
@@ -9,8 +9,6 @@ public class yseries {
         int divCount = 0;
 
         if (N % 2 == 0) {
-            int factor = 0;
-            
             for (int i = 1; i <= N; i++) {
                 if (N % i == 0) {
                     divCount++;
