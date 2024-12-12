00b2b0088662b445be2149751d093e4a877ed90f
snadwich <79296594+snadwich01@users.noreply.github.com>
2024-12-12 15:26:43 +0600

    Create Task 1.java

---
+++ Lab stuff/Assignment 5/Task 1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class caseConvert {
+  public static void main (String [] args) {
+    
+    Scanner data = new Scanner(System.in);
+    
+    System.out.println("Please enter the conversion characters");
+    String theWord = data.nextLine();
+    String output = "";
+    
+    //print uppercase
+    
+    for (int idk = 0; idk < theWord.length(); idk++){
+      char blk = theWord.charAt(idk);
+      if (blk >= 'A' && blk<= 'Z') {
+        output += (char)(((int)(blk))+32);
+
+      } else{
+        output += blk;
+      }
+    }
+     System.out.println(output);
+   
+    data.close();
+  }}
