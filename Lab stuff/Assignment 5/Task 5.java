94046fc5fb10a12acb99e451aac57c2d9bdf145a
snadwich <79296594+snadwich01@users.noreply.github.com>
2024-12-18 00:49:57 +0600

    Update Task 5.java

--- Lab stuff/Assignment 5/Task 5.java
+++ Lab stuff/Assignment 5/Task 5.java
@@ -1,29 +1,35 @@
 import java.util.Scanner;
 
-public class caseConvert {
-  public static void main (String [] args) {
-    
-    Scanner data = new Scanner(System.in);
-    
-    System.out.println("Please enter your message");
-    String msg = data.nextLine();
-    int vowelCount = 0;
-    int consCount = 0;
-    
-    for (int idk = 0; idk < msg.length(); idk++) {
-      char blk = msg.charAt (idk);
-      
-      if (blk >= 'A' && blk <= 'Z' || blk >= 'a' && blk <= 'z'){
-        if (blk == 'a' || blk == 'e' || blk == 'i' || blk == 'o' || blk == 'u' || blk == 'A' || blk == 'E' || blk == 'I' || blk == 'O' || blk == 'U'){
-          vowelCount++;
-        } else{
-           //consonant 
-          consCount++;
-        }
-      }
+public class treasuree {
 
-      System.out.println(consCount);
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+
+        System.out.print("Your message? : ");
+        String msg = data.nextLine();
+
+        int vowelCount = 0;
+        int consCount = 0;
+
+        for (int idk = 0; idk < msg.length(); idk++) {
+            char letter = msg.charAt(idk);
+
+            if ((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z')) {
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||
+                    letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U') {
+                    vowelCount++;
+                } else {
+                    consCount++;
+                }
+            }
+        }
+
+        if (vowelCount > 0 && consCount > 0 && vowelCount % 3 == 0 && consCount % 5 == 0) {
+            System.out.println("Aarr! Me Plunder!!");
+        } else {
+            System.out.println("Blimey! No Plunder!!");
+        }
+
+        data.close();
     }
-   
-    data.close();
-  }}
+}
